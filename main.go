0c99271eaa0f85600bb2650c2315027ce1bafd24
Jerry Young <jerryyoung2911@gmail.com>
2021-11-29 22:24:16 +0700

    Fitur Penjumlahan

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	a := 5
+	b := 5
 	fmt.Println("Hello world")
 	fmt.Println("Update 1")
 	fmt.Println("Update 2")
@@ -10,4 +12,5 @@ func main() {
 	fmt.Println("Update 4")
 	fmt.Println("Update 5")
 	fmt.Println("Fitur-1 Commit-1")
+	fmt.Println(a + b)
 }
