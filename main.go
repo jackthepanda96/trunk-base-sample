85d6de0ef3bd6c407e3e904643780b12c07616e8
Jerry Young <jerryyoung2911@gmail.com>
2021-11-30 09:15:54 +0700

    After cherry-pick

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-=======
 	a := 5
 	b := 5
 	c := 5
 	d := 5
->>>>>>> bdf76e7 (Merge branch 'fitur-2')
 	fmt.Println("Hello world")
 	fmt.Println("Update 1")
 	fmt.Println("Update 2")
@@ -17,8 +14,6 @@ func main() {
 	fmt.Println("Update 4")
 	fmt.Println("Update 5")
 	fmt.Println("Fitur-1 Commit-1")
-<<<<<<< HEAD
-=======
 	fmt.Println(a + b)
 	fmt.Println("Update 6")
 	fmt.Println("Update 7")
@@ -26,5 +21,4 @@ func main() {
 	fmt.Println("Fitur-2 Commit-1")
 	fmt.Println(c / d)
 	fmt.Println("Fitur-2 Commit-2")
->>>>>>> bdf76e7 (Merge branch 'fitur-2')
 }
